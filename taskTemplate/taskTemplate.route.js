1acc97ae7c7ee79ccd2ed2677996379547815410
karishmaMohammed <karishmamohammed43@gmail.com>
2024-10-02 13:31:12 +0530

    task template comtroller done

--- taskTemplate/taskTemplate.route.js
+++ taskTemplate/taskTemplate.route.js
@@ -4,9 +4,9 @@ const { authenticateToken } = require('../middlewares/authenticator');
 
 const router = express.Router();
 
-router.route('/create-form-ele').post(authenticateToken, taskTempCtrl.createFormFields);
+router.route('/create-form-ele').post(taskTempCtrl.createFormFields);
 router.route('/get-form-ele').get(authenticateToken, taskTempCtrl.getFormFields);
-router.route('/create-default-fields').post(authenticateToken, taskTempCtrl.createDefaultFields);
+router.route('/create-default-fields').post(taskTempCtrl.createDefaultFields);
 router.route('/create-task-template').post(authenticateToken, taskTempCtrl.createTaskTemplate);
 router.route('/get-task-template').get(authenticateToken, taskTempCtrl.getTaskTemplate);
 
