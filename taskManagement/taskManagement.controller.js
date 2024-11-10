41cc49ec428957fbc4dadd069ae9ae3f018f347c
karishmaMohammed <karishmamohammed43@gmail.com>
2024-11-10 17:06:47 +0530

    pagination

--- taskManagement/taskManagement.controller.js
+++ taskManagement/taskManagement.controller.js
@@ -173,7 +173,7 @@ async function getTaskList(req, res) {
   let responseData;
   try {
     const page = parseInt(req.query.page) || 1;  
-    const limit = parseInt(req.query.limit) || 10; 
+    const limit = parseInt(req.query.limit) || 8; 
     const skip = (page - 1) * limit;
     const search = req.query.search || ""; 
     const result = await taskManagementModel.aggregate([
